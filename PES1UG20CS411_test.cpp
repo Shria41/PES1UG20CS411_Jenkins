b2097c1eba418c92dbc6facae61e4b1de8a2cf0b
Shria41 <78139587+Shria41@users.noreply.github.com>
2023-02-17 15:01:18 +0530

    Create PES1UG20CS411_test.cpp

---
+++ PES1UG20CS411_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG20CS411\n";
+  cout << "Shria Guntunur\n";
+  cout << "I have successfully built and run Jenkins pipeline\n";
+  return 0;
+}
