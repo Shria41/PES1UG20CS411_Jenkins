874d3d7c091aaec8cf86a40efb1e23535426ac4a
Shria41 <78139587+Shria41@users.noreply.github.com>
2023-02-17 09:13:48 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES1UG20CS411\n";
+  cout << "Shria Guntunur!\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
